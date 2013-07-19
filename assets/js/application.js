10b992e988ad6d34ebd2c38eff8c51218dd9fe64
Nicole Dominguez <sodevious.net@gmail.com>
2013-07-19 11:59:04 -0400

    carousel, more styles, work

---
+++ assets/js/application.js
@@ -0,0 +1,26 @@
+$(function() {
+    $( "#about" ).tabs({
+        event: "mouseover",
+        show: { effect: "fade", duration: 400 }
+    });
+});
+
+$(function() {
+    $( "#process" ).tabs({
+        event: "mouseover",
+        show: { effect: "fade", duration: 400 }
+    });
+});
+
+$(function() {
+    $( "#carousel ").rcarousel({
+        margin: 10,
+        visible: 2,
+        step: 2,
+     width: 295,
+     height: 350
+    });
+
+    $( "#ui-carousel-next" )
+    .add( "#ui-carousel-prev" )
+});
